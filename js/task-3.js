24e347984fcbf7b846a5eed6f43b448bfa5887d5
AnnBalias <annbalias34@gmail.com>
2024-10-12 20:09:34 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+    const more = [];
+   for (const num of numbers) {
+     if (num > value) {
+         more.push(num);
+     } 
+    }
+    return more;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
